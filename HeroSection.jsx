ebb6951e81dd1a3fc15cd0858eb4bc8c76d36af3
AbuBakkorSiddique <153945310+AbuBakkorSiddique@users.noreply.github.com>
2024-07-11 01:18:20 +0600

    add two file only

---
+++ HeroSection.jsx
@@ -0,0 +1,38 @@
+import '../css/heroSection.css'
+
+function HeroSection() {
+  return (
+ <>
+ 
+<div className="main bg-success">
+  {/* --Team Player  Tam : A ---------------- -*/}
+  <div className="sub1"></div>
+  <div className="sub2"></div>
+  <div className="sub3"></div>
+  <div className="sub4"></div>
+  <div className="sub5"></div>
+  <div className="sub6"></div>
+  <div className="sub7"></div>
+  <div className="sub8"></div>
+  <div className="sub9"></div>
+  <div className="sub10"></div>
+  <div className="sub11"></div>
+
+
+  {/*-------- D-Box */}
+  <div className="D-Box"></div>
+  <div className="D-Box-2"></div>
+  {/* ----Middle point Design --------- */}
+  <div className="middle"></div>
+  <div className="middle-round bg-danger"></div>
+  {/* ---------- Bar ------------------ */}
+  <div className="bar"></div>
+  <div className="bar2"></div>
+
+</div>
+ 
+ </>
+  )
+}
+
+export default HeroSection
